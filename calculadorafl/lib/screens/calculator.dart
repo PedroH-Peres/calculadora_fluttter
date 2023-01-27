8e340c4c0e108fb36633bf77d10a96aa55b5b81d
Pedro Henrique B. Peres <pedrohperescode@gmail.com>
2023-01-26 23:58:06 -0300

    base

---
+++ calculadorafl/lib/screens/calculator.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Calculator extends StatelessWidget {
+  const Calculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Column(
+        children: [
+          Text("Display"),
+          Text("Keyboard")
+        ],
+      ),
+    );
+  }
+}
